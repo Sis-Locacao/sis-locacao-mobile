ad68dee482e7801b3d4c6f9a0b718ccbc5830cae
WilliamWJD <william.jose.wjd@gmail.com>
2023-06-22 16:28:58 -0300

    feat: configuração inicial do projeto

---
+++ src/theme/index.ts
@@ -0,0 +1,37 @@
+import { extendTheme } from "native-base";
+
+export const THEME = extendTheme({
+  colors:{
+    green:{
+      700: "#16A085",
+      600: "#1ABC9C",
+      500: "#24D0AE"
+    },
+
+    white: "#ffffff",
+    steelBlue: "#34495E",
+
+    gray:{
+      700: "#B4BBBA",
+      600: "#BDC3C7",
+      500: "#A4ABB1"
+    },
+
+    red: "#E24444"
+  },
+  fonts: {
+    heading: "Inter_400Regular",
+    body: "Inter_700Bold",
+  },
+  fontSizes: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 30,
+  },
+  sizes: {
+    14: 56,
+    33: 148,
+  },
+});
